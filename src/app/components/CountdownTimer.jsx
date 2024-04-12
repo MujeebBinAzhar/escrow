129abb23d94605bd891428dfc78342991d724aca
MujeebBinAzhar <52318178+MujeebBinAzhar@users.noreply.github.com>
2024-04-12 11:33:54 +0500

    timer

--- src/app/components/CountdownTimer.jsx
+++ src/app/components/CountdownTimer.jsx
@@ -1,14 +1,15 @@
 "use client"
 
  
-
 import React, { useEffect, useState } from 'react';
 import Countdown from 'react-countdown';
 
 const CountdownTimer = () => {
+  const fixedEndingDate = new Date('2024-04-17T00:00:00').getTime();
+
   const [targetDate, setTargetDate] = useState(() => {
     const storedTargetDate = localStorage.getItem('targetDate');
-    return storedTargetDate ? parseInt(storedTargetDate, 10) : Date.now() + 432000000; // Default to 5 days from now if no stored date
+    return storedTargetDate ? parseInt(storedTargetDate, 10) : fixedEndingDate; // Default to fixed ending date if no stored date
   });
 
   useEffect(() => {
