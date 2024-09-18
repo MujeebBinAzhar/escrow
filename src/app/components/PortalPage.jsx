b19dc8e1975caa18018384c90a1c0c3d9a397e7a
mujeebbinazhar <shamimujeeb623@gmail.com>
2024-09-18 21:58:10 +0500

    new added

--- src/app/components/PortalPage.jsx
+++ src/app/components/PortalPage.jsx
@@ -103,7 +103,7 @@ const PortalPage = () => {
                 </p>
                 <div className="progresss-bar">
                   <div className="progresss-bar-inner">
-                    <span>81%</span>
+                    <span>96%</span>
                   </div>
                 </div>
               </div>
@@ -186,7 +186,7 @@ const PortalPage = () => {
                 </p>
                 <div className="progresss-bar">
                   <div className="progresss-bar-inner">
-                    <span>81%</span>
+                    <span>96%</span>
                   </div>
                 </div>
               </div>
